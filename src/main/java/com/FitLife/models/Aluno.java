ccf48ca77c4904067cf90910094d65f9fd6a77bd
Pedro Henrique Teles <pedrohenriqueteles732@gmail.com>
2024-06-14 00:14:03 -0300

    model Aluno criado

---
+++ src/main/java/com/FitLife/models/Aluno.java
@@ -0,0 +1,58 @@
+package com.FitLife.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.lang.annotation.Inherited;
+import java.util.Date;
+
+@Document(collection = "alunos")
+public class Aluno {
+
+    @Id
+    private Long id;
+    private String nome;
+    private String sexo;
+    private int idade;
+    private Date dataInscricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Date getDataInscricao() {
+        return dataInscricao;
+    }
+
+    public void setDataInscricao(Date dataInscricao) {
+        this.dataInscricao = dataInscricao;
+    }
+}
