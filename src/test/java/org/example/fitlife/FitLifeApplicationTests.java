b530fd0693d6a66aff1e88692b3ff2c347897eb2
Pedro Henrique Teles <pedrohenriqueteles732@gmail.com>
2024-06-13 23:52:14 -0300

    First commit

---
+++ src/test/java/org/example/fitlife/FitLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fitlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
