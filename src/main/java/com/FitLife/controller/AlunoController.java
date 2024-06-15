018b5664f3b88e3bf0bb5a30cecd5f6e3e626d5b
Pedro Henrique Teles <pedrohenriqueteles732@gmail.com>
2024-06-14 22:25:42 -0300

    ControllerAluno Criado

--- src/main/java/com/FitLife/controller/AlunoController.java
+++ src/main/java/com/FitLife/controller/AlunoController.java
@@ -1,4 +1,56 @@
 package com.FitLife.controller;
 
+import com.FitLife.models.Aluno;
+import com.FitLife.models.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @GetMapping
+    public List<Aluno> getAllAlunos() {
+        return alunoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Aluno> getAlunoById(@PathVariable Long id) {
+        return alunoRepository.findById(id)
+                .map(aluno -> ResponseEntity.ok().body(aluno))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Aluno createAluno(@RequestBody Aluno aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Aluno> updateAluno(@PathVariable Long id, @RequestBody Aluno alunoUpdate) {
+        return alunoRepository.findById(id)
+                .map(aluno -> {
+                    aluno.setNome(alunoUpdate.getNome());
+                    aluno.setSexo(alunoUpdate.getSexo());
+                    aluno.setIdade(alunoUpdate.getIdade());
+                    Aluno updatedAluno = alunoRepository.save(aluno);
+                    return ResponseEntity.ok().body(updatedAluno);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteAluno(@PathVariable Long id) {
+        return alunoRepository.findById(id)
+                .map(aluno -> {
+                    alunoRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
 }
