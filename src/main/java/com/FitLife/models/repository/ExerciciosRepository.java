7b47db0e3aeb6bb1da798621e112c6c6705d9bc8
Pedro Henrique Teles <pedrohenriqueteles732@gmail.com>
2024-06-19 19:54:31 -0300

    Service de consulta, criado para realizar as validações de cada consulta

--- src/main/java/com/FitLife/models/repository/ExerciciosRepository.java
+++ src/main/java/com/FitLife/models/repository/ExerciciosRepository.java
@@ -2,6 +2,7 @@ package com.FitLife.models.repository;
 
 import com.FitLife.models.Exercicios;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.Duration;
@@ -16,6 +17,5 @@ public interface ExerciciosRepository extends MongoRepository<Exercicios, String
     List<Exercicios> findByIntensidade (String intensidade);
     List<Exercicios> findByDataBetween (Date dataInicio, Date dataFim);
     List<Exercicios> findByDataAfter (Date data);
-
 
 }
